54e34e8a2ace42763105b8b880c90655387be3da
CodeVirggin <78057753+ambbrose@users.noreply.github.com>
2023-02-23 01:57:19 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import SwitchButton from './switch'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/switch' element={<SwitchButton />} />
+      </Routes>
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
